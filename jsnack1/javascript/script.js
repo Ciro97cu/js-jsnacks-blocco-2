f1c2d8cfc9e025e81bdeb3804bf4653375340c69
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-11 13:13:39 +0100

    completed first snack

--- jsnack1/javascript/script.js
+++ jsnack1/javascript/script.js
@@ -1,2 +1,16 @@
-let prova = "Everything is Working";
-console.log(prova)
+/*Crea un array di numeri interi e fai la somma di tutti
+gli elementi che sono in posizione dispari.*/
+
+// array list of number
+const numberList = [1, 2, 3, 4, 5, 6, 7, 20, 4, 33, 50];
+
+// declared variable used to make the sum
+let sumOddNumebrs = 0;
+for (let i = 0; i < numberList.length; i++) {
+    // condition to select only numbers in odd positions
+    if (i % 2 === 0) {
+        sumOddNumebrs += numberList[i];
+    }
+}
+// console log of the sum of our numbers in odd positions
+console.log(sumOddNumebrs);
